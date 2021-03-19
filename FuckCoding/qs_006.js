3bb636aad283d5e1cabc45346181517682f049a3
len <len.hong@ubtrobot.com>
2021-03-19 15:48:29 +0800

    qs_007

--- FuckCoding/qs_006.js
+++ FuckCoding/qs_006.js
@@ -1,3 +1,4 @@
+// 有一个嵌套层次很深的对象，key 都是 a_b 形式 ，需要改成 ab 的形式，注意不能用递归。
 const a = {
   a_y: {
     a_z: {
@@ -17,11 +18,37 @@ const a = {
 
 // 简单快捷，但是 不是很可靠的方法
 
-function remove_(obj) {
-  const string = JSON.stringify(obj).replace(/_/g,"")
-  const json = JSON.parse(string)
-  console.log(json);
-  return string;
-}
+// function remove_(obj) {
+//   const string = JSON.stringify(obj).replace(/_/g,"")
+//   const json = JSON.parse(string)
+//   console.log(json);
+//   return string;
+// }
 
 remove_(a)
+
+const obj: any = {
+  a_y: {
+    a_z: {
+      y_x: 6
+    },
+    b_c: 1
+  }
+}
+
+// 利用栈或者队列来处理
+const bfs = (root) => {
+  const arr = [root];
+  while(arr.length) {
+    const current = arr.shift();
+    for (let key of Object.keys(current)) {
+      const changedKey = key.replace('_', '')
+      current[changedKey] = current[key]
+      delete current[key]
+      arr.push(current[changedKey])
+    }
+  }
+  console.log('result: ', root)
+}
+
+bfs(a)
---
+++ FuckCoding/qs_007.js
@@ -0,0 +1,41 @@
+// 请实现一个大数相加函数，比如说 1111111111111111111 + 1，直接数字相加答案是错误的。
+
+// 另请说明为什么在 JS 会存在这样的问题？
+
+/*
+
+原因：如果是整数的范围的话，JavaScript 能够准确表示的整数范围在-2^53 到 2^53 之间（不含两个端点）， 超过这个范围，无法精确表示这个整数
+*/
+
+const a = "81111111111111111119";
+const b = "61111111111111111119";
+
+const bigIntAdd = (a, b) => {
+  console.log(a);
+  console.log(b);
+  const arrA = a.split("").map((i) => parseInt(i));
+  const arrB = b.split("").map((i) => parseInt(i));
+  const result = [];
+  let overNum = 0;
+
+  while (arrA.length > 0 || arrB.length > 0) {
+    const a = arrA.pop() || 0;
+    const b = arrB.pop() || 0;
+    let sum = a + b;
+    if (overNum) {
+      sum += 1;
+      overNum = 0;
+    }
+    if (sum > 9) {
+      result.unshift(sum - 10);
+      overNum = 1;
+    } else {
+      result.unshift(sum);
+    }
+  }
+
+  if (overNum) result.unshift(overNum);
+  return result.join().replace(/,/g, "");
+};
+
+console.log(bigIntAdd(a, b));
